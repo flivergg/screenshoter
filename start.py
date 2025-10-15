e71480b4bb05e0c2bc9471c34513a528030a4e4f
Mr_Fliver <mrfliver255@gmail.com>
2025-10-15 15:34:01 +0300

    start.py

---
+++ start.py
@@ -0,0 +1,9 @@
+# Запустите этот файл чтобы проверить работу программы
+# перед компиляцией в EXE
+
+import screenshot_app
+
+
+if __name__ == "__main__":
+    app = screenshot_app.ScreenshotApp()
+    app.run()
